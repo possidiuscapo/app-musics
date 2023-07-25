376faf551c2adcf488ad02ebb4c4383aefe4b80c
Possidius Capo <possidiusc@gmail.com>
2023-07-25 12:01:48 +0100

    album service

--- src/app/album-details/album-details.component.ts
+++ src/app/album-details/album-details.component.ts
@@ -1,7 +1,7 @@
 // Component enfant
 // la classe Input est nécessaire
 
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import { Album, List } from '../album';
 import { ALBUM_LISTS } from "../mock-albums";
 
@@ -10,12 +10,13 @@ import { ALBUM_LISTS } from "../mock-albums";
   templateUrl: './album-details.component.html',
   styleUrls: ['./album-details.component.css']
 })
-export class AlbumDetailsComponent implements OnInit {
+export class AlbumDetailsComponent implements OnInit, OnChanges {
   // Classe Input permet de récupérer les data de l'enfant
   // album est liée à une entrée [album] du parent dans le sélecteur
   @Input() album!: Album;
+  @Output() onPlay : EventEmitter <Album> = new EventEmitter();
   tabDeList!: string[] | undefined;
-
+  albumLists: List[] = ALBUM_LISTS;
   constructor() { }
 
   ngOnInit() {
@@ -23,13 +24,20 @@ export class AlbumDetailsComponent implements OnInit {
   }
 
   ngOnChanges() {
+    // if(this.album !== undefined){
+    //   ALBUM_LISTS.forEach((albumList) => {
+    //     if ( albumList.id === this.album.id) {
+    //       this.tabDeList = albumList.list;
+    //     }
+    //   });
+    // }
+
     if(this.album !== undefined){
-      ALBUM_LISTS.forEach((albumList) => {
-        if ( albumList.id === this.album.id) {
-          this.tabDeList = albumList.list;
-        }
-      });
+      this.tabDeList = this.albumLists.find((elem) => elem.id === this.album.id)?.list;
     }
   }
-
+  play(album:Album){
+    this.onPlay.emit(album)
+    console.log("Appel de la methode Play")
+  }
 }
---
+++ src/app/album.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlbumService {
+
+  constructor() { }
+
+  getAlbums(){}
+  getAlbum(id: string){}
+  getAlbumList(id: string){}
+}
