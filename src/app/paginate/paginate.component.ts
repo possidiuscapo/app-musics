cdbebc3a344c70466026a2ac5b3f9f61e7b69530
Possidius Capo <possidiusc@gmail.com>
2023-07-31 15:19:39 +0100

    modification

--- src/app/paginate/paginate.component.ts
+++ src/app/paginate/paginate.component.ts
@@ -6,27 +6,27 @@ import { AlbumService } from '../album.service';
   templateUrl: './paginate.component.html',
   styleUrls: ['./paginate.component.css']
 })
-export class PaginateComponent implements OnInit{
+export class PaginateComponent implements OnInit {
   //nombre totale d'albums
   totale: number = 0;
   // Numbre d'album par page (stocke dans les variables d'environments)
-  perPage: number ;
+  perPage: number;
 
-  numberPages :number = 0;
+  numberPages: number = 0;
 
-  pages :number[] =[]
+  pages: number[] = []
 
   //emetteur d'évènement 
 
-  @Output() setPaginate: EventEmitter<{start :number, end: number}> = new EventEmitter;
+  @Output() setPaginate: EventEmitter<{ start: number, end: number }> = new EventEmitter;
 
   //variable qui stock la page actuelle
 
-  currentPage : number  = 1; 
+  currentPage: number = 1;
 
   constructor(
-    private albumService : AlbumService
-  ){
+    private albumService: AlbumService
+  ) {
     this.perPage = this.albumService.paginateNumberPage();
   }
 
@@ -35,29 +35,29 @@ export class PaginateComponent implements OnInit{
     this.totale = this.albumService.count()
     this.numberPages = Math.ceil(this.totale / this.perPage)
     for (let i = 1; i <= this.numberPages; i++) {
-       this.pages.push(i); 
+      this.pages.push(i);
     }
   }
 
-  next(){
+  next() {
     // this.currentPage++;
-    if(this.currentPage >= this.numberPages){
+    if (this.currentPage >= this.numberPages) {
       // this.currentPage = 1
       return;
-    }else{
+    } else {
       this.currentPage++
     }
     this.setPaginate.emit(this.setAlbums(this.currentPage))
-    
+
     console.log(this.currentPage);
   };
-  
-  previous(){
+
+  previous() {
     // this.currentPage--;
-    if(this.currentPage <= 1){
+    if (this.currentPage <= 1) {
       // this.currentPage = 1
       return;
-    }else{
+    } else {
       this.currentPage--
     }
     this.setPaginate.emit(this.setAlbums(this.currentPage))
@@ -67,14 +67,15 @@ export class PaginateComponent implements OnInit{
   /**
    * function qui retourne le sous ensemble d'albums à afficer
    */
-   setAlbums(page: number): {start: number, end: number}{
+  setAlbums(page: number): { start: number, end: number } {
     let start = (page - 1) * this.perPage
     let end = start + this.perPage
 
     // return {start : start, end : start}
-    return {start, end}
-   }
-    changePage(page: number ){
-      this.setPaginate.emit(this.setAlbums(page))
-    }
+    return { start, end }
+  }
+  changePage(page: number) {
+    this.currentPage = page;
+    this.setPaginate.emit(this.setAlbums(this.currentPage))
+  }
 }
