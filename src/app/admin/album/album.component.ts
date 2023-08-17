a1e2a397c6f6f9782d49a13b71254eb7b11b4d01
Possidius Capo <possidiusc@gmail.com>
2023-08-17 09:09:05 +0100

    Pagination de l'album

--- src/app/admin/album/album.component.ts
+++ src/app/admin/album/album.component.ts
@@ -8,11 +8,23 @@ import { AlbumService } from 'src/app/album.service';
   styleUrls: ['./album.component.css']
 })
 export class AlbumComponent {
-  albums!: Album[] ;
-  constructor(private aS: AlbumService) {}
+  albums!: Album[];
+  constructor(private aS: AlbumService, public albumService: AlbumService) { }
   ngOnInit(): void {
     this.aS.getAlbums().subscribe({
-      next: (alb: Album[]) => {this.albums = alb}
+      next: (alb: Album[]) => { this.albums = alb }
+    });
+    this.albumService
+      .paginate(0, this.albumService.paginateNumberPage())
+      .subscribe({
+        next: (alb: Album[]) => { this.albums = alb }
+      })
+  }
+  onSetPaginate($event: { start: number, end: number }) {
+    // Récupérer les albums compris entre [start et end]
+    this.albumService.paginate($event.start, $event.end)
+    .subscribe({
+      next: (al: Album[]) => this.albums = al
     })
-}
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    numberPage : 3,
+    numberPage : 4,
     albumUrl:  ' http://localhost:3000/albums',
     albumListUrl:  'http://localhost:3000/albumList'
 };
